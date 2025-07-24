95ac99415423fbb9c3a866c3cc58e4b65ed4874d
skrsikop <skrsikop@gmail.com>
2025-07-24 10:58:42 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/yt-clone",
+  base: '/', // ✅ This is 100% safe for Vercel
 })
